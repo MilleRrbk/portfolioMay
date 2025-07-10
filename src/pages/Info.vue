062fd79ff3d399d7773f868235cfe7fdf70583f3
Mille Trantel Rørbæk <143701353+MilleRrbk@users.noreply.github.com>
2025-07-11 00:24:28 +0200

    info

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -9,7 +9,7 @@
             <p>Mit navn er Mille Rørbæk. <br>Jeg brænder for at designe og skabe visuelle fortællinger og universer, der kan mærkes. <br>Jeg er draget af det, der vækker følelser, glæde eller eftertanke. <br>Jeg har en fascination for det mere surrealistiske og det skæve, som vækker nysgerrighed og sætter fanatsien i gang.</p>
           </div>
           <h2 class="page-title1"> Min uddannelse</h2>
-          <p>Jeg er i gang med at færdiggøre min professionsbachelor i Coded Design - Visuel Kommunikation på DMJX, hvor jeg får lov til at udfolde mig kreativt og bringe mine idéer til live. <br>Uddannelsen kombinerer design, teknologi og konceptudvikling, og jeg arbejder med digitale brugeroplevelser, kode og visuel formgivning.<br> Jeg har udviklet projekter, der spænder fra interaktive websites og 3D universer til typografi og visuel identitet. <br>Der lægges vægt på processer og ekserimentering, og det har lært mig at tænke kritisk, visuelt og tværfagligt.</p>
+          <p>Jeg er i gang med at færdiggøre min professionsbachelor i Coded Design - Visuel Kommunikation på DMJX, hvor jeg får lov til at udfolde mig kreativt og bringe mine idéer til live. <br>Uddannelsen kombinerer design, teknologi og konceptudvikling, og jeg arbejder med digitale brugeroplevelser, kode og visuel formgivning.<br> Jeg har udviklet projekter, alt fra interaktive websites og 3D universer til typografi og visuel identitet. <br>Der lægges vægt på processer og ekserimentering, og det har lært mig at tænke kritisk og visuelt.</p>
         </div>
         <div class="info-image">
           <img src="@/assets/imgs/meee.JPG" alt="portræt af mille">
@@ -25,6 +25,9 @@
     </p>
     <p>
       Telefon: 27 57 40 53
+    </p>
+    <p class="link-styling">
+      <a href="www.linkedin.com/in/mille-rørbæk-13a774176">LinkedIn</a>
     </p>
       </div>
       </section>
